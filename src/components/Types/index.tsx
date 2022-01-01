af54a38648df2a3fed4c3d4891e854d0342a1d83
Vitalii Malashevskyi <malashevsky.v@gmail.com>
2022-01-01 22:55:08 +0200

    update colors

--- src/components/Types/index.tsx
+++ src/components/Types/index.tsx
@@ -23,29 +23,209 @@ const Types = () => {
     <Box as="section" maxW="500px" fontSize="15px" mx="auto" mb={7}>
       <Tabs onChange={onChangeTabsHandler} defaultIndex={2}>
         <TabList alignItems="center">
-          <Tab>R G B</Tab>
-          <Tab>R G B A</Tab>
-          <Tab>X Y R G B</Tab>
-          <Tab>X Y R G B A</Tab>
+          <Tab>
+            <Box color="red">R</Box>&nbsp;<Box color="green.700">G</Box>&nbsp;
+            <Box color="blue.600">B</Box>
+          </Tab>
+          <Tab>
+            <Box color="red">R</Box>&nbsp;<Box color="green.700">G</Box>&nbsp;
+            <Box color="blue.600">B</Box>&nbsp;
+            <Box color="black">A</Box>
+          </Tab>
+          <Tab>
+            <Box color="black">X</Box>&nbsp;<Box color="black">Y</Box>&nbsp;
+            <Box color="red">R</Box>&nbsp;<Box color="green.700">G</Box>&nbsp;
+            <Box color="blue.600">B</Box>
+          </Tab>
+          <Tab>
+            <Box color="black">X</Box>&nbsp;<Box color="black">Y</Box>&nbsp;
+            <Box color="red">R</Box>&nbsp;<Box color="green.700">G</Box>&nbsp;
+            <Box color="blue.600">B</Box>&nbsp;
+            <Box color="black">A</Box>
+          </Tab>
           <Tab>X Y</Tab>
           <PopoverInfo />
         </TabList>
         <TabPanels>
           <TabPanel>
-            <Badge>(r, g, b)</Badge>&nbsp;
-            <Badge>[[123, 111, 153], [...], ...]</Badge>
+            <Badge>
+              (
+              <Box as="span" color="red">
+                r
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                g
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                b
+              </Box>
+              )
+            </Badge>
+            &nbsp;
+            <Badge>
+              [[
+              <Box as="span" color="red">
+                123
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                111
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                153
+              </Box>
+              ], [...], ...]
+            </Badge>
           </TabPanel>
           <TabPanel>
-            <Badge>(r, g, b, a)</Badge>&nbsp;
-            <Badge>[[123, 111, 153, 0.89], [...], ...]</Badge>
+            <Badge>
+              (
+              <Box as="span" color="red">
+                r
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                g
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                b
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                a
+              </Box>
+              )
+            </Badge>
+            &nbsp;
+            <Badge>
+              [[
+              <Box as="span" color="red">
+                123
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                111
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                153
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                0.89
+              </Box>
+              ], [...], ...]
+            </Badge>
           </TabPanel>
           <TabPanel>
-            <Badge>(x, y, r, g, b)</Badge>&nbsp;
-            <Badge>[[34, 20, 123, 111, 153], [...], ...]</Badge>
+            <Badge>
+              (
+              <Box as="span" color="black">
+                x
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                y
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="red">
+                r
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                g
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                b
+              </Box>
+              )
+            </Badge>
+            &nbsp;
+            <Badge>
+              [[
+              <Box as="span" color="black">
+                34
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                20
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="red">
+                123
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                111
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                153
+              </Box>
+              ], [...], ...]
+            </Badge>
           </TabPanel>
           <TabPanel>
-            <Badge>(x, y, r, g, b, a)</Badge>&nbsp;
-            <Badge>[[34, 20, 123, 111, 153, 0.89], [...], ...]</Badge>
+            <Badge>
+              (
+              <Box as="span" color="black">
+                x
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                y
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="red">
+                r
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                g
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                b
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                a
+              </Box>
+              )
+            </Badge>
+            &nbsp;
+            <Badge>
+              [[
+              <Box as="span" color="black">
+                34
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                20
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="red">
+                123
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="green.700">
+                111
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="blue.600">
+                153
+              </Box>
+              ,&nbsp;
+              <Box as="span" color="black">
+                0.89
+              </Box>
+              ], [...], ...]
+            </Badge>
           </TabPanel>
           <TabPanel>
             <Badge>(x, y)</Badge>&nbsp;
